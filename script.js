3c918f10817193d7e598ba9c4d65efa3a6ba19f7
Natalie Garcia <gnatalie.booking@gmail.com>
2020-07-12 16:42:49 -0400

    finished html, added js, added moment js day to html header

---
+++ script.js
@@ -0,0 +1,15 @@
+//calling by ID 
+var dayDiv = $("#currentDay");
+
+//showing date in header
+var formatDay = moment(formatDay).format("dddd, MMMM Do YYYY");
+
+$(dayDiv).text(formatDay);
+
+
+
+// moment("09:00", "h:mm a", true).format()
+
+// if( )
+
+
