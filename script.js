6c0e23d7f234623acc5f3d50122295486e3bb245
Natalie Garcia <gnatalie.booking@gmail.com>
2020-07-16 16:52:58 -0400

    changing the rows to javascript, adding a function to build a row, adding a function to build a table with a for loop from the row, adding if statements to change the colors of the rows

--- script.js
+++ script.js
@@ -1,74 +1,47 @@
+//global variables
+var dayDiv = $("#currentDay");
+var dayContainer = $('.container');
 
-    //calling by ID 
-    var dayDiv = $("#currentDay");
-    var textEntryOne = $("#text-entry1").val(window.localStorage.getItem("text-entry1"));
-    var textEntryTwo = $("#text-entry2");
-    var textEntryThree = $("#text-entry3");
-    var textEntryFour = $("#text-entry4");
-    var textEntryFive = $("#text-entry5");
-    var textEntrySix = $("#text-entry6");
-    var textEntrySeven = $("#text-entry7");
-    var textEntryEight = $("#text-entry8");
-    var textEntryNine = $("#text-entry9");
+// show date in header
+var formatDay = moment().format("dddd, MMMM Do YYYY");
+dayDiv.text(formatDay);
 
-    // buttons
-    var btnOne = $("#save1");
+//calling function to build hour table
+buildTable();
 
-    // user input var
-    var userInputOne = [];
-    
-    //showing date in header
-    var formatDay = moment(formatDay).format("dddd, MMMM Do YYYY");
-    var time = moment(time).format("hh:mm a");
-    console.log(time);
-    $(dayDiv).text(formatDay);
-    
-     // color coding times 
-     var nine = moment(nine).format("09: a");
-     console.log(nine);
-     var ten = moment(ten).format("10:00 a");
-     var eleven = moment(eleven).format("11:00 a");
-     var twelve = moment(twelve).format("12:00 p");
-     var one = moment(one).format("01:00 p");
-     var two = moment(two).format("02:00 p");
-     var three = moment(three).format("08: p");
-     var four = moment(four).format("04:00 p");
-     var five = moment(five).format("05:00 p");
-    
-    // 9am div
-     if(time > nine) {
-         $(textEntryOne).css("background-color", "gray");
-     }
-     if(time === nine) {
-        $(textEntryOne).css("background-color", "red");
-     }
-     if(time < nine) {
-        $(textEntryOne).css("background-color", "green");
-     }
-
-    // 10am div
-     if(time > ten) {
-        $(textEntryTwo).css("background-color", "gray");
+//  for loop to build table from row
+function buildTable() {
+    for (var i = 8; i <= 17; i++) {
+        buildRow(i)
     }
-   
-//11am div
-    if(time > eleven) {
-        $(textEntryThree).css("background-color", "gray");
+}
+
+// building a row for each hour
+function buildRow(hour) {
+    var timeObj = moment(hour, "H")
+    var parent = $('<div class="row time-block">')
+    var timeClass = "past"
+
+    if (moment().isSame(timeObj, 'hour')) {
+        timeClass = "present"
     }
-//12pm div
-    if(time > twelve) {
-        $(textEntryFour).css("background-color", "gray");
+    else if (moment().isBefore(timeObj, "hour")) {
+        timeClass = "future"
     }
-// 3pm div test
-    if(time < three) {
-        $(textEntrySeven).css("background-color", "green");
-        console.log(time);
-     }
-     
-//button
-     $(btnOne).click(function(event){
-         event.preventDefault();
-         window.localStorage.setItem(textEntryOne, textEntryOne.value);
-        
-        });
-        console.log(textEntryOne);
+
+    var time = $('<div class="hour col-1">')
+    time.text(timeObj.format("h A"))
+
+    var textarea = $('<textarea id="" class="description col-10">')
+    textarea.addClass(timeClass)
+    textarea.val(schedule[hour])
+
+    var saveBtn = $('<button class="saveBtn col-1">')
+    saveBtn.click(handleClick)
+
+    var icon = $('<i class="fas fa-save">')
+
+    saveBtn.append(icon);
+    parent.append(time, textarea, saveBtn)
+    dayContainer.append(parent)
+};
