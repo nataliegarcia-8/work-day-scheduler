73770164b108a6b2e3bbf3a7bc232dd890d06511
Natalie Garcia <gnatalie.booking@gmail.com>
2020-07-16 23:53:51 -0400

    fixed local storage, text inputs saves after refresh, minor css changes

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # work-day-scheduler-
+
+A work day scheduler to organize your busy work days by the hour.
+Link: <a href=""> Click to view Work Day Scheduler</a>
+
+
+Description: A work day scheduler to add events, track the hours, and manage the work day. Using HTML, CSS, Bootstrap, JavaScript, JQuery, local storage, and moment.js to create a basic work day planner. The user can navigate through the scheduler through past, present, and future color coded for each timeblock into the scheduler based on the current time. The user can click on each timeblock to add events to the work day. The user can click the save button to save the event in the scheduler. 
+
+
+If you need support please email me at:
+gnatalie.booking@gmail.com
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //global variables
 var dayDiv = $("#currentDay");
 var dayContainer = $('.container');
-
+$(document).ready(function(){
 // show date in header
 var formatDay = moment().format("dddd, MMMM Do YYYY");
 dayDiv.text(formatDay);
@@ -12,36 +12,56 @@ buildTable();
 //  for loop to build table from row
 function buildTable() {
     for (var i = 8; i <= 17; i++) {
-        buildRow(i)
+        buildRow(i);
     }
 }
 
 // building a row for each hour
 function buildRow(hour) {
-    var timeObj = moment(hour, "H")
-    var parent = $('<div class="row time-block">')
-    var timeClass = "past"
-
+    // variables 
+    var timeObj = moment(hour, "H");
+    var parent = $('<div class="row time-block">');
+    var timeClass = "past";
+    
+    // assigning colors to each row to signify past, present, or future 
     if (moment().isSame(timeObj, 'hour')) {
-        timeClass = "present"
+        timeClass = "present";
     }
     else if (moment().isBefore(timeObj, "hour")) {
-        timeClass = "future"
+        timeClass = "future";
     }
-
+    // time div 
     var time = $('<div class="hour col-1">')
-    time.text(timeObj.format("h A"))
+    time.text(timeObj.format("h A"));
+    
+    //textarea 
+    var textarea = $('<textarea id="user-input" class="description col-10">')
+    textarea.addClass(timeClass);
+    textarea.attr("data-value", hour);
+    
+    // saving text on refresh
+    var storedItem = textarea.text(localStorage.getItem(hour));
+    console.log(storedItem);
 
-    var textarea = $('<textarea id="" class="description col-10">')
-    textarea.addClass(timeClass)
-    // textarea.val(schedule[hour])
-
-    var saveBtn = $('<button class="saveBtn col-1">')
-    // saveBtn.click(handleClick)
-
-    var icon = $('<i class="fas fa-save">')
-
-    saveBtn.append(icon);
-    parent.append(time, textarea, saveBtn)
-    dayContainer.append(parent)
+    // creating save button
+    var saveBtn = $('<button class="saveBtn col-1">');
+    saveBtn.text("SAVE");
+    
+    // setting divs to parent containers 
+    parent.append(time, textarea, saveBtn);
+    dayContainer.append(parent);
+    
+    // save 
+    saveBtn.on("click", function (event) {
+        var thisBtn = $(this);
+        console.log(thisBtn);
+        var textarea = $(this).prev();
+        
+        var hour = textarea.attr("data-value");
+        
+        event.preventDefault();
+        localStorage.setItem(hour, textarea.val());
+        
+    });
 };
+});
